2ad533f2bfd6a3ede54518b5b3482d661d6d9c1a
Takashi Mori <kanetugu2015@gmail.com>
2026-01-07 11:42:17 +0900

    refactored

--- include/hakoniwa/api/iservice_handler.hpp
+++ include/hakoniwa/api/iservice_handler.hpp
@@ -3,6 +3,7 @@
 #include "hakoniwa/api/protocol.hpp"
 #include "hakoniwa/pdu/rpc/rpc_services_server.hpp"
 #include "hakoniwa/api/server_context.hpp"
+#include <atomic> // Added for std::atomic
 
 namespace hakoniwa::api {
 
@@ -14,12 +15,12 @@ public:
         is_canceled_ = true;
     }
     virtual bool is_canceled() const {
-        return is_canceled_;
+        return is_canceled_.load(); // Use .load() for atomic read
     }
     virtual void reset_canceled() {
         is_canceled_ = false;
     }
 protected:
-    bool is_canceled_{false};
+    std::atomic<bool> is_canceled_{false}; // Changed to std::atomic<bool>
 };
 } // namespace hakoniwa::api

aaf7a671e00ad3e4de3a0292b29da6375098de09
Takashi Mori <kanetugu2015@gmail.com>
2026-01-08 10:29:46 +0900

    add main

---
+++ src/main/client.cpp
@@ -0,0 +1,51 @@
+#include "hakoniwa/api/client_core.hpp"
+
+int main(int argc, const char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <path_to_remote_api.json> <nodeId>" << std::endl;
+        return 1;
+    }
+    std::string config_path = argv[1];
+    std::string node_id = argv[2];
+    // Example usage of ClientCore
+    hakoniwa::api::ClientCore client(node_id, config_path);
+    if (!client.initialize()) {
+        std::cerr << "Client initialization failed: " << client.last_error() << std::endl;
+        return 1;
+    }
+    if (!client.start()) {
+        std::cerr << "Client start failed: " << client.last_error() << std::endl;
+        return 1;
+    }
+    while (!client.is_pdu_end_point_running()) {
+        std::cout << "Waiting for PDU endpoint to be running..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    // keyboard input for api calls
+    std::cout << "Client joined the simulation. Press Enter to stop..." << std::endl;
+    while (true) {
+        // get keyboard input
+        std::string input;
+        std::getline(std::cin, input);
+        // Join the simulation
+        if (input == "join") {
+            if (!client.join()) {
+                std::cerr << "Join failed: " << client.last_error() << std::endl;
+            } else {
+                std::cout << "Joined the simulation successfully." << std::endl;
+            }
+        }
+        else if (input == "q" || input == "quit" || input == "exit") {
+            std::cout << "Exiting..." << std::endl;
+            break;
+        }
+    }
+    
+    if (!client.stop()) {
+        std::cerr << "Client stop failed: " << client.last_error() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
---
+++ src/main/server.cpp
@@ -0,0 +1,29 @@
+#include "hakoniwa/api/server_core.hpp"
+
+
+int main(int argc, const char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <path_to_remote_api.json> <server_nodeId>" << std::endl;
+        return 1;
+    }
+    // Example usage of ServerCore
+    hakoniwa::api::ServerCore server(argv[1], argv[2], true);
+    if (!server.initialize()) {
+        std::cerr << "Server initialization failed: " << server.last_error() << std::endl;
+        return 1;
+    }
+    if (!server.start()) {
+        std::cerr << "Server start failed: " << server.last_error() << std::endl;
+        return 1;
+    }
+
+    // Run the server for some time (e.g., 10 seconds)
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    if (!server.stop()) {
+        std::cerr << "Server stop failed: " << server.last_error() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
